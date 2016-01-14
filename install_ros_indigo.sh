04d1317a6eb81cf211e6d5f61dce4f99696b2721
Luc Bettaieb <luc.bettaieb@gmail.com>
2016-01-14 14:25:37 -0500

    added install_ros_indigo.sh

---
+++ install_ros_indigo.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Luc Bettaieb
+# ROS Installation
+# v 0.1
+
+echo "Beginning ROS Installation"
+
+echo -e "\e[1m \e[34m >>> Beginning ROS Indigo Installation \e[21m \e[39m"
+echo -e "\e[34m >>> Setting up sources.list and keys... \e[39m"
+
+  sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+  sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+
+echo -e "\e[34m >>> ...done\e[39m"
+
+  sudo apt-get update
+
+echo -e "\e[34m >>> Beginning ros-indigo-desktop-full installation...\e[39m"
+
+  sudo apt-get --yes --force-yes install ros-indigo-desktop-full 
+
+echo -e "\e[34m >>> Setting up rosdep\e[39m"
+
+  sudo rosdep init
+  rosdep update
+
+echo -e "\e[34m >>> Setting up environment \e[39m"
+
+  echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+  source ~/.bashrc
+
+echo -e "\e[34m >>> Setting up rosinstall \e[39m"
+
+  sudo apt-get --yes --force-yes install python-rosinstall
+
+  source ~/.bashrc
+
+echo -e "\e[34m >>> done! \e[39m"
