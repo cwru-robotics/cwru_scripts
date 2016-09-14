dfec6bc05fcb53ebfd67bda6209becbad674abab
biocubed <rcj33@case.edu>
2016-09-14 12:21:34 -0400

    I added a script to install kinetic-kane, as well as a generic workspace generation script. All scripts are now executable

--- install_ros_kinetic.sh
+++ install_ros_kinetic.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Luc Bettaieb
+# Luc Bettaieb & Russell Jackson
 # ROS Installation
 # v 0.1
 
 echo "Beginning ROS Installation"
 
-echo -e "\e[1m \e[34m >>> Beginning ROS Indigo Installation \e[21m \e[39m"
+echo -e "\e[1m \e[34m >>> Beginning ROS Kinetic Installation \e[21m \e[39m"
 echo -e "\e[34m >>> Setting up sources.list and keys... \e[39m"
 
   sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -15,9 +15,9 @@ echo -e "\e[34m >>> ...done\e[39m"
 
   sudo apt-get update
 
-echo -e "\e[34m >>> Beginning ros-indigo-desktop-full installation...\e[39m"
+echo -e "\e[34m >>> Beginning ros-kinetic-desktop-full installation...\e[39m"
 
-  sudo apt-get --yes --force-yes install ros-indigo-desktop-full 
+  sudo apt-get --yes --force-yes install ros-kinetic-desktop-full 
 
 echo -e "\e[34m >>> Setting up rosdep\e[39m"
 
@@ -26,7 +26,7 @@ echo -e "\e[34m >>> Setting up rosdep\e[39m"
 
 echo -e "\e[34m >>> Setting up environment \e[39m"
 
-  echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+  echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
   source ~/.bashrc
 
 echo -e "\e[34m >>> Setting up rosinstall \e[39m"
